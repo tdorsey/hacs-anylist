6ab00a7eda9f11ab81b92917cc1418a6c8108cf5
tdorsey <tdorsey@users.noreply.github.com>
2025-07-13 11:30:47 -0400

    Feature/recipe integration (#12)

--- custom_components/anylist/coordinator.py
+++ custom_components/anylist/coordinator.py
@@ -1,40 +1,5 @@
 """Define an object to manage fetching AnyList data."""
 
-from datetime import timedelta
-
-from homeassistant.exceptions import UpdateFailed
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-
-from .const import DOMAIN, LOGGER
-
-
-class AnylistUpdateCoordinator(DataUpdateCoordinator):
-    """Define an object to manage fetching AnyList data."""
-
-    def __init__(self, hass, config_entry, list_name, refresh_interval):
-        """Initialize the AnyList data coordinator."""
-        super().__init__(
-            hass,
-            LOGGER,
-            config_entry=config_entry,
-            name=f"Anylist {list_name}",
-            update_interval=timedelta(minutes=refresh_interval),
-        )
-        self.list_name = list_name
-        self.hass = hass
-
-    async def _async_update_data(self):
-        """Fetch data from AnyList."""
-        code, items = await self.hass.data[DOMAIN].get_detailed_items(self.list_name)
-        if code != 200:
-            LOGGER.error(
-                "Failed to fetch data for list '%s': code %s", self.list_name, code
-            )
-            raise UpdateFailed(
-                f"Error fetching data for list '{self.list_name}': code {code}"
-            )
-        return items
-=======
 from __future__ import annotations
 
 from abc import abstractmethod
@@ -75,6 +40,14 @@ class AnyListData:
     shoppinglist_coordinator: "AnyListShoppingListCoordinator"
     statistics_coordinator: "AnyListStatisticsCoordinator"
     recipe_coordinator: "AnyListRecipeCoordinator"
+
+
+@dataclass
+class ShoppingListData:
+    """Data class for shopping list data."""
+
+    shopping_list: ShoppingList
+    items: list[ShoppingItem]
 
 
 AnyListConfigEntry = ConfigEntry[AnyListData]
@@ -138,14 +111,6 @@ class AnyListMealplanCoordinator(
         for meal in response.items:
             res[meal.entry_type].append(meal)
         return res
-
-
-@dataclass
-class ShoppingListData:
-    """Data class for shopping list data."""
-
-    shopping_list: ShoppingList
-    items: list[ShoppingItem]
 
 
 class AnyListShoppingListCoordinator(
